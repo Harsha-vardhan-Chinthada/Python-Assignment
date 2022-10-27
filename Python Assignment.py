ba5cd85387f9ac7096d29425d37944e3f0b340f9
Harsha-vardhan-Chinthada <harshavardhan14008c010@gmail.com>
2022-10-27 23:38:27 +0530

    We should declare the type of data for input we are taking.

---
+++ Python Assignment.py
@@ -0,0 +1,7 @@
+class_started = bool(int(input("Hey friend, is class started?: [0-False/1-True]")))
+if class_started:
+   print("Since class started...")
+   print("Lets concentrate")
+else:
+   print("Since class is not started...")
+   print("let's revise") 
